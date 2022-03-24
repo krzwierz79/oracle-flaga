222608b9fe410111ad531e23cf90c5a607f7611f
krzwierz79 <krzwierz79@gmail.com>
2022-03-24 12:52:40 +0100

    gaps init

--- app.py
+++ app.py
@@ -3,34 +3,47 @@ import random
 from programs.wiki_search import wiki_search
 from programs.wiki_hero_search import heroes_sorted
 from programs.ffriends import read_ffriend
+from programs.gaps import build_exercise
 
 
-app=Flask(__name__)
+app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     text = open('dane/xd.txt').read()
     return render_template("index.html", text=text)
 
+
 @app.route('/flaga-dla-ukrainy')
 def ukr():
-    ukrainian = random.choice(['Ukrainian Falcons', 'Ukrainian Levkoy', 'Ukrainian alphabet'])
+    ukrainian = random.choice(
+        ['Ukrainian Falcons', 'Ukrainian Levkoy', 'Ukrainian alphabet'])
     from_wiki = wiki_search(ukrainian).encode('utf-8').decode()
     ua_heroes = heroes_sorted()
-    pl_word, ua_word, pl_mean, ua_mean = read_ffriend('/var/www/flaga/dane/ffriends_pl_ua.csv')
-    return render_template("ukraine.html", 
-    ua_heroes = ua_heroes,
-    to_page = from_wiki,
-    pl_word = pl_word, 
-    ua_word = ua_word, 
-    pl_mean = pl_mean, 
-    ua_mean = ua_mean)
+    pl_word, ua_word, pl_mean, ua_mean = read_ffriend(
+        '/var/www/flaga/dane/ffriends_pl_ua.csv')
+    return render_template("ukraine.html",
+                           ua_heroes=ua_heroes,
+                           to_page=from_wiki,
+                           pl_word=pl_word,
+                           ua_word=ua_word,
+                           pl_mean=pl_mean,
+                           ua_mean=ua_mean)
 
 
-    
+@app.route('/gaps')
+def gaps():
+    box, passage = build_exercise()
+    return render_template("gaps.html",
+                           box=box,
+                           passage=passage)
+
+
 @app.route('/xd')
 def xd():
     return render_template("xd.html")
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     app.run()
---
+++ programs/gaps.py
@@ -0,0 +1,69 @@
+import random
+import re
+
+passage = "The Python creator and Dropbox engineer reflects on his early days in programming. Guido van Rossum is the creator and benevolent dictator for life BDFL of the Python programming language. Here he reflects on his path and shares what he's been working on since joining Dropbox. Read on for this and a hint at what's next for Dropbox, for Python, and for the BDFL himself.\n You were an electronics hobbyist before becoming a programmer. How did you get started in electronics? \n Okay, we're going waaaay back. I don't know exactly why I got into electronics as a kid. I remember that from the last grade in elementary school and probably through my second year in university, electronics kits and my own designs were one of my big passions. \n It wasn't always an easy path.\n In elementary school, I took one of the first projects I built into class as a show and tell project. There was no one who understood what it was or why it was interesting or cared. It's a very vague memory. I just know that I took it in, and it fell flat."
+
+words_to_check = ["creator", "benevolent", "vague",
+                  "hobbyist", "kits", "path", "Voldemort"]
+
+# removed_words = []
+
+
+# using regex( findall() )
+# to extract words from string
+# words = re.findall(r'\w+', passage)
+
+# printing result
+# print("\nThe words of string are")
+# for i in words:
+#     print(i)
+
+
+def splitting_sentences(text: str, split_by: str):
+    text = [i.strip() for i in text.split(split_by) if i]
+    return text
+
+
+split_passage = splitting_sentences(passage, ".")
+
+
+def replace_str(word_list: list, sentence_num: int):
+    chosen_word = ''
+    while len(chosen_word) < 2:
+        chosen_word = word_list[random.randint(0, len(word_list)-1)]
+    for word in words_to_check:
+        if word in word_list:
+            chosen_word = word
+    char_count = len(chosen_word)
+    replacement = f"({sentence_num}){' _' * char_count} "
+    return [chosen_word, replacement]
+
+
+def build_exercise():
+    removed_words = []
+    gapped_passage = []
+
+    for count, sentence in enumerate(split_passage):
+        # print(f"{count} zdanie: {sentence}")
+        removed, replacement = replace_str(
+            splitting_sentences(sentence, " "), count + 1)
+        gapped_sentence = f"{sentence.replace(removed, replacement, 1)}."
+        removed_words.append(removed)
+        random.shuffle(removed_words)
+        gapped_passage.append(gapped_sentence)
+
+        # print(f"zdanie z luką: {gapped_sentence}")
+    print(
+        f"usunięto {len(removed_words)} słów: {removed_words} z {len(gapped_passage)} zdań.")
+    # random_words = list(set(removed_words) - set(words_to_check))
+    # unchecked_words = list(set(words_to_check) - set(removed_words))
+    # print(f"losowe słowa: {random_words}")
+    # print(f"niesprawdzone słowa: {unchecked_words}")
+    return [removed_words, gapped_passage]
+
+
+build_exercise()
+# print(build_exercise())
+
+# todo - wyszukać niesprawdzone słowa w innych tekstach (wikipedia?) pobrać je i dołączyć do zdań
+# todo2 - dodać obsługę zwrotów zamiast pojedynczych słów (np "used to" a nie tylko "used")
