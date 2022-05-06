52c983aa7646888c22b8692bffb4b9b46765d5f6
krzysiekwie <krzysiekwie@gmail.com>
2022-05-06 08:31:12 +0000

    docstrings

--- app.py
+++ app.py
@@ -23,6 +23,13 @@ def index():
 
 @app.route('/flaga-dla-ukrainy')
 def ukr():
+    """
+    Return data for Ukraine-themed page
+    
+		ua_heroes - wiki search by local script, sorted by length of descr
+    to_page/from_wiki - simple wiki-search for random element from 'ukraininan' list
+    pl/ua_word/mean - random choice od pl/ua true/false-friend words from csv
+    """
     ukrainian = random.choice(
         ['Ukrainian Falcons', 'Ukrainian Levkoy', 'Ukrainian alphabet'])
     from_wiki = wiki_search(ukrainian).encode('utf-8').decode()
@@ -40,6 +47,12 @@ def ukr():
 
 @app.route('/gaps')
 def gaps():
+    """
+    Return a passage of text with words removed and given in a box
+    (fill in the gaps exercise)
+    TODO: add a form to select some words for removal (other gaps are randomized)
+    TODO: add a possibility to select more texts
+    """
     box, passage = build_exercise()
     return render_template("gaps.html",
                            box=box,
@@ -48,36 +61,38 @@ def gaps():
 
 @app.route('/xd')
 def xd():
+    """Basic template render"""
     return render_template("xd.html")
 
 # 029 intro to storing data (in plain text) 
 
 @app.route('/flaga', methods=["GET", "POST"])
 def flaga():
-	# Set up folder structure for data
-	create_folders()
+    """Uses internal list/s of names, stores wiki-search on them, returns stored data"""
+    create_folders() # Set up folder structure for data
 
-	# Flag count number of flag images
-	flag_count = len(os.listdir('static/flag_image')) 
-	# pick a random flag
-	# xd = random.choice(range(1, flag_count))
-	# if flag_count < 2:
-	# 	xd = 1
-	xd = random.randint(1, flag_count) # seems simpler than above
+	  # Flag count number of flag images
+    flag_count = len(os.listdir('static/flag_image')) 
+	  # pick a random flag
+	  # xd = random.choice(range(1, flag_count))
+	  # if flag_count < 2:
+	  # 	xd = 1
+    xd = random.randint(1, flag_count) # seems simpler than above
 	
-	# hard-code a flag (testing)
-	# ta_flaga = os.path.join(app.config['UPLOAD_FOLDER'], 'flag_image/Flaga__2.jpg')
-	# use a rangom flag
-	ta_flaga = os.path.join(app.config['UPLOAD_FOLDER'], 'flag_image/Flaga__{}.jpg'.format(xd))
+	  # hard-code a flag (testing)
+	  # ta_flaga = os.path.join(app.config['UPLOAD_FOLDER'], 'flag_image/Flaga__2.jpg')
+	  # use a rangom flag
+    ta_flaga = os.path.join(app.config['UPLOAD_FOLDER'], 'flag_image/Flaga__{}.jpg'.format(xd))
 	
-	# scrape/store/read heroes data 
-	heroes = gather_heroes(xd)
-	random.shuffle(heroes)
+	  # scrape/store/read heroes data 
+    heroes = gather_heroes(xd)
+    random.shuffle(heroes)
 
-	return render_template("flaga.html", xd=xd, flaga=ta_flaga, heroes=heroes)
+    return render_template("flaga.html", xd=xd, flaga=ta_flaga, heroes=heroes)
 
 def gather_heroes(xd):
-	patriots = [
+
+    patriots = [
  		'Mikołaj Kopernik', 
  		'Rotmistrz Pilecki',
  		'Maria Skłodowska',
@@ -94,88 +109,86 @@ def gather_heroes(xd):
  		# 'Mieczysław Paluch',
 	]
 
-	pirates = [
+    pirates = [
  		'Anne Bonny', 
  		'Czarnobrody',
  		'Sir Francis Drake',
  		'Henry Morgan',
 	]
 
-	# pick side (flag file and heroes)
-	if (xd % 2) == 0:
-		heroes = pirates # even num in static/flga_image/Flaga__
-		hero_tag = 'pirate'
-	else:
-		heroes = patriots # odd
-		hero_tag = 'patriot'
+    # pick side (flag file and heroes)
+    if (xd % 2) == 0:
+        heroes = pirates # even num in static/flga_image/Flaga__
+        hero_tag = 'pirate'
+    else:
+        heroes = patriots # odd
+        hero_tag = 'patriot'
 
-	# TODO: not used on the web
-	greetings = [
+    # TODO: not used on the web
+    greetings = [
 		'pozdrawia',
 		'/wave',
 		'/wink',
 		'wita',
 	]
 
-	# pull new data from wikipedia
-	wikipedia.set_lang("pl")
+	  # pull new data from wikipedia
+    wikipedia.set_lang("pl")
 
-	saved_heroes = os.listdir('dane/heroes/saved_heroes')
-	saved_heroes = [h.split('.')[0] for h in saved_heroes]
+    saved_heroes = os.listdir('dane/heroes/saved_heroes')
+    saved_heroes = [h.split('.')[0] for h in saved_heroes]
 
-	for hero in heroes:
-		if hero not in saved_heroes:
+    for hero in heroes:
+        if hero not in saved_heroes:
 
-			# Get some info and link.
-			some_info = wikipedia.page(hero)
-			info_intro = some_info.content.split('\n\n')[0]
-			url = '<a href="'+some_info.url+'">Poszukaj więcej info o: '+hero+"</a>"
+        # Get some info and link.
+            some_info = wikipedia.page(hero)
+            info_intro = some_info.content.split('\n\n')[0]
+            url = '<a href="'+some_info.url+'">Poszukaj więcej info o: '+hero+"</a>"
+						# Get what hero thinks. (query wikiquotes)
+            hero_think(hero)
 			
-			# Get what hero thinks. (query wikiquotes)
-			hero_think(hero)
-			
-			# Get & save images. TODO: not used
-			# images = some_info.images
-			# n_photos = 0
-			# for i, image_url in enumerate(images):
-			# 	if i < 3:
-			# 		hero_str = '11'.join(hero.split())
-			# 		image_name = '{}_{}.legend'.format(hero_str, i)
-			# 		save_image(image_url, image_name)
-			# 		n_photos += 1
+						# Get & save images. TODO: not used
+						# images = some_info.images
+						# n_photos = 0
+						# for i, image_url in enumerate(images):
+						# 	if i < 3:
+						# 		hero_str = '11'.join(hero.split())
+						# 		image_name = '{}_{}.legend'.format(hero_str, i)
+						# 		save_image(image_url, image_name)
+						# 		n_photos += 1
 
-			# Save all data into file with patriot or pirate ext (side/hero_tag)
-			with open('dane/heroes/saved_heroes/'+hero+"."+hero_tag, "w+") as f:
-				f.write(hero + '\n')
-				f.write('\n') #str(n_photos) + '\n')
-				f.write(info_intro + '\n')
-				f.write(url)
+            # Save all data into file with patriot or pirate ext (side/hero_tag)
+            with open('dane/heroes/saved_heroes/'+hero+"."+hero_tag, "w+") as f:
+                f.write(hero + '\n')
+                f.write('\n') #str(n_photos) + '\n')
+                f.write(info_intro + '\n')
+                f.write(url)
 
-		else:
-			# TODO: not used on the web - display info here already exists? 
-			greeting = random.choice(greetings)
-			print(hero, greeting)
+        else:
+            # TODO: not used on the web - display info here already exists? 
+            greeting = random.choice(greetings)
+            print(hero, greeting)
 
-	heroes = []
-	# read all heroes
-	for hero_file in os.listdir('dane/heroes/saved_heroes'):
-		# select files for chosen side
-		if hero_file.endswith(hero_tag):
-				# build dict with data to display
-				hero = {}
-				some_info = open('dane/heroes/saved_heroes/'+hero_file).readlines()
-				hero['name'] = some_info[0]
-				#photo_nr = random.choice(range(int(some_info[1])))
-				#hero_str = '11'.join(hero['name'][:-1].split())
-				#hero['image'] = '{}_{}.legend'.format(hero_str, photo_nr)
-				hero_quotes = open('dane/heroes/hero_think/' + hero['name'][:-1] + ".hero").readlines()
-				hero['quote'] = random.choice(hero_quotes)
-				hero['description'] = '\n'.join(some_info[2:-1])
-				hero['description'] = bold(hero['description'])
-				hero['url'] = some_info[-1]
-				heroes.append(hero)
-
-	return heroes
+        heroes = []
+        # read all heroes
+        for hero_file in os.listdir('dane/heroes/saved_heroes'):
+            # select files for chosen side
+            if hero_file.endswith(hero_tag):
+            # build dict with data to display
+                hero = {}
+                some_info = open('dane/heroes/saved_heroes/'+hero_file).readlines()
+                hero['name'] = some_info[0]
+                #photo_nr = random.choice(range(int(some_info[1])))
+                #hero_str = '11'.join(hero['name'][:-1].split())
+                #hero['image'] = '{}_{}.legend'.format(hero_str, photo_nr)
+                hero_quotes = open('dane/heroes/hero_think/' + hero['name'][:-1] + ".hero").readlines()
+                hero['quote'] = random.choice(hero_quotes)
+                hero['description'] = '\n'.join(some_info[2:-1])
+                hero['description'] = bold(hero['description'])
+                hero['url'] = some_info[-1]
+                heroes.append(hero)
+    return heroes
 
 # def save_image(image_url, image_name):
 # 	image = requests.get(image_url).content
